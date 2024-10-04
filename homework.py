62d0d58c15929fbdefe7eaeffc688f0129368710
Ivan Tkachenko <frost19812014@gmail.com>
2024-10-04 18:53:29 +0300

    forth vertion

--- homework.py
+++ homework.py
@@ -33,7 +33,7 @@ def check_tokens():
         logging.debug("Проверка наличия токенов пройдена успешно")
     else:
         logging.critical("Отсутствие обязательных переменных окружения"
-            f" во время запуска бота: {absent_tokens}")
+                         f" во время запуска бота: {absent_tokens}")
         sys.exit(1)
 
 
